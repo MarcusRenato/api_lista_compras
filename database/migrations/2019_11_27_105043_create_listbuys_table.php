770ada185c6f53be0108a24f929186846f2be023
Marcus Renato <marcusrenato@live.com>
2019-11-28 21:15:25 -0300

    Adicionando migrate correta

--- database/migrations/2019_11_27_105043_create_listbuys_table.php
+++ database/migrations/2019_11_27_105043_create_listbuys_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateListbuysTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('listbuys', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('description');
-            $table->float('price');
-            $table->string('image');
+            $table->string('category');
             $table->timestamps();
         });
     }
